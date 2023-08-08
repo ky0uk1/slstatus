76689bef7539cd541373653106a1510c800c4d5b
ky0uk1 <gabrielraraujo456@gmail.com>
2023-08-08 14:17:43 -0300

    Adding Cpu and RAM percentage, bytes on the net and date

--- config.h
+++ config.h
@@ -65,5 +65,8 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
+	{ cpu_perc, "[CPU: %s%%]  ", NULL	},
+	{ ram_perc, "[RAM: %s%%]  ", NULL	},
+	{netspeed_rx, "%sB/s ", "enp0s3"	},
+	{ datetime, "%s",           "%F %T" 	},
 };
